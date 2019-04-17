debeb23e46bc3508fb5ac1883b18711644355726
Vladimir Kalmykov <vladimirmorulus@gmail.com>
2019-04-17 18:30:04 +0300

    Support refs

--- dist/reflex.js
+++ dist/reflex.js
@@ -4,6 +4,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -22,17 +24,30 @@ var _contextTypes2 = _interopRequireDefault(_contextTypes);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
 var reflex = function reflex(Component) {
-  var Reflex = function Reflex(props, context) {
+  var Reflex = function Reflex(_ref, context) {
+    var reflexProxyRef = _ref.reflexProxyRef,
+        props = _objectWithoutProperties(_ref, ['reflexProxyRef']);
+
     var config = Object.assign({}, _config2.default, context.reflexbox);
     var next = (0, _css2.default)(config)(props);
 
-    return _react2.default.createElement(Component, next);
+    return _react2.default.createElement(Component, _extends({}, next, {
+      ref: reflexProxyRef
+    }));
   };
 
   Reflex.contextTypes = _contextTypes2.default;
 
-  return Reflex;
+  var ReflexRefForwarder = _react2.default.forwardRef(function (props, ref) {
+    return _react2.default.createElement(Reflex, _extends({}, props, {
+      reflexProxyRef: ref
+    }));
+  });
+
+  return ReflexRefForwarder;
 };
 
 exports.default = reflex;
--- src/reflex.js
+++ src/reflex.js
@@ -3,17 +3,25 @@ import css from './css'
 import defaultConfig from './config'
 import contextTypes from './context-types'
 
-const reflex = Component => {
-  const Reflex = (props, context) => {
+const reflex = (Component) => {
+  const Reflex = ({ reflexProxyRef, ...props }, context) => {
     const config = Object.assign({}, defaultConfig, context.reflexbox)
     const next = css(config)(props)
 
-    return React.createElement(Component, next)
+    return React.createElement(Component, {
+      ...next,
+      ref: reflexProxyRef,
+    })
   }
 
   Reflex.contextTypes = contextTypes
 
-  return Reflex
+  const ReflexRefForwarder = React.forwardRef((props, ref) => (<Reflex
+    {...props}
+    reflexProxyRef={ref}
+  />))
+
+  return ReflexRefForwarder
 }
 
 export default reflex
